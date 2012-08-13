e3a254ad82cbc71bbaacc79a38039f6fbc080401
shleger <sh2a@yandex.ru>
2012-08-14 02:10:19 +0400

    add callback

---
+++ projects/big-data-threader/src/main/java/ru/saa/thread/FeedBack.java
@@ -0,0 +1,17 @@
+package ru.saa.thread;
+
+public class FeedBack {
+
+	long currentPosition;
+
+	public long getCurrentPosition() {
+		return currentPosition;
+	}
+
+	public void setCurrentPosition(long currentPosition) {
+		this.currentPosition = currentPosition;
+	}
+
+
+
+}
